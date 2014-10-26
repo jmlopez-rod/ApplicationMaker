2fd01c122d6e1f5d52c69224f058a047b4d493b8
smnpaorg <smnpaorg>
2014-10-26 04:43:15 -0400

    $ApplicationsDirectory & PacletInfo.m
    Added $ApplicationsDirectory doc and updated indeces with PacletInfo.m

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,38 +1,21 @@
-Paclet[
-	Name -> "ApplicationMaker",
-	Version -> "0.0.1",
-	MathematicaVersion -> "8+",
-	Extensions -> {
-		{
-			"Kernel",
-			"Context" -> {
-	"ApplicationMaker`ApplicationMaker`", 
-				"ApplicationMaker`DocumentationMaker`", 
-				"ApplicationMaker`PackageMaker`", 
-
-			}
-		},
-		{
-			"Documentation",
-			Language -> "English",
-			LinkBase -> "ApplicationMaker",
-			Resources -> {
-				"Guides/ApplicationMaker",
-				"Tutorials/HowToBuildADocumentedPackage",
-		"ReferencePages/Symbols/BuildApplication",
-				"ReferencePages/Symbols/CreateReferencePages",
-				"ReferencePages/Symbols/DeployApplication",
-				"ReferencePages/Symbols/EditNotebookOption",
-				"ReferencePages/Symbols/EditOption",
-				"ReferencePages/Symbols/NewApplication",
-				"ReferencePages/Symbols/NewGuide",
-				"ReferencePages/Symbols/NewPackage",
-				"ReferencePages/Symbols/NewTutorial",
-				"ReferencePages/Symbols/UpdateInit",
-				"ReferencePages/Symbols/ViewNotebookOption",
-				"ReferencePages/Symbols/ViewOption",
-
-			}
-		}
-	}
-]
+(* Created with the Wolfram Language : www.wolfram.com *)
+Paclet[Name -> "ApplicationMaker", Version -> "0.0.1", 
+ MathematicaVersion -> "8+", Extensions -> 
+  {{"Kernel", "Context" -> {"ApplicationMaker`ApplicationMaker`", 
+      "ApplicationMaker`DocumentationMaker`", 
+      "ApplicationMaker`PackageMaker`"}}, {"Documentation", 
+    Language -> "English", LinkBase -> "ApplicationMaker", 
+    Resources -> {"Guides/ApplicationMaker", 
+      "Tutorials/HowToBuildADocumentedPackage", 
+      "ReferencePages/Symbols/BuildApplication", 
+      "ReferencePages/Symbols/CreateReferencePages", 
+      "ReferencePages/Symbols/DeployApplication", 
+      "ReferencePages/Symbols/EditNotebookOption", 
+      "ReferencePages/Symbols/EditOption", 
+      "ReferencePages/Symbols/NewApplication", 
+      "ReferencePages/Symbols/NewGuide", "ReferencePages/Symbols/NewPackage", 
+      "ReferencePages/Symbols/NewTutorial", 
+      "ReferencePages/Symbols/UpdateInit", 
+      "ReferencePages/Symbols/ViewNotebookOption", 
+      "ReferencePages/Symbols/ViewOption", 
+      "ReferencePages/Symbols/$ApplicationsDirectory"}}}]
